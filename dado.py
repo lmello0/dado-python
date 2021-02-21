229f5b3a7af7907a9aa2fc7003f34f6a6fc2315e
lmello0 <lmello0@github.com>
2021-02-21 13:56:32 -0300

    Update dado.py

--- dado.py
+++ dado.py
@@ -1,5 +1,6 @@
 from random import randint
 i = 1
+c = 1
 
 while i == 1:
     jogar = str(input('Quer jogar o dado [S/N]: ')).upper()
@@ -8,12 +9,16 @@ while i == 1:
 
 while jogar == 'S':
     i = 1
-    qtd = int(input('Quer jogar um dado de quantos lados? '))
+    while c == 1:
+        qtd = int(input('Quer jogar um dado de quantos lados? '))
+        if qtd > 0:
+            c = 0
     dado = randint(1, qtd)
 
     print('-=-' * 10)
     print('Valor do dado: {}'.format(dado))
     print('-=-' * 10)
+    c = 1
     while i == 1:
         jogar = str(input('Quer jogar o dado [S/N]: ')).upper()
         if jogar == 'S' or jogar == 'N':
